2fde16067f96c60c3ad40a62ccb8e5c1a5f016aa
lyl@YL <146@qq.com>
2025-09-12 12:22:15 +0800

    尝试项目修改

--- helloworld/helloworld/src/helloworld/__main__.py
+++ helloworld/helloworld/src/helloworld/__main__.py
@@ -1,4 +1,14 @@
-import helloworld.TestClock
+# import helloworld.TestClock
+# import helloworld.TestClass1
+# import helloworld.togo_demo2
+import helloworld.togo_demo3
+# import helloworld.togo_demo4
+# import helloworld.togo_demo5
 
 if __name__ == "__main__":
-    helloworld.TestClock.main()
+    # helloworld.TestClock.main()
+    # helloworld.TestClass1.main().main_loop()
+    # helloworld.togo_demo2.main().main_loop()
+    helloworld.togo_demo3.main().main_loop()
+    # helloworld.togo_demo4.main().main_loop()
+    # helloworld.togo_demo5.main().main_loop()
